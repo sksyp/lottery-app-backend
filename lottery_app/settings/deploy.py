fff8c67644ebe3a06afcf177bdf91b2f73307545
ARVINDKUMAR, SHORYA <shorya.arvindkumar@nissanmotor.com>
2021-01-15 23:56:14 +0530

    adding database details for deployment

--- lottery_app/settings/deploy.py
+++ lottery_app/settings/deploy.py
@@ -4,10 +4,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "SCHEMA": "public",
-        "NAME": "lottery_app",
-        "USER": "postgres",
-        "PASSWORD": "admin",
-        "HOST": "127.0.0.1",
+        "NAME": "ddctqlacpin9v",
+        "USER": "puuweeclvjrssi",
+        "PASSWORD": "55e52f150c97c8f585b71ccd0929f57d444bb29ec99b25e154fb108acc645a97",
+        "HOST": "ec2-18-208-49-190.compute-1.amazonaws.com",
         "PORT": "5432",
     }
 }
